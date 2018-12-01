4a418acc0205f198f8871c98a28d2530e6d917e4
mjarkk <mkopenga@gmail.com>
2018-12-01 17:25:48 +0100

    Reduced the amound of oses that this program compiles for

--- release/main.go
+++ release/main.go
@@ -114,7 +114,7 @@ func Compile() {
 			Run(goGet, projectDir)
 
 			// Build the go program
-			gox := "gox -output ./release/tempGoBuild/{{.Dir}}_{{.OS}}_{{.Arch}}"
+			gox := "gox -os windows -os linux -os darwin -output ./release/tempGoBuild/{{.Dir}}_{{.OS}}_{{.Arch}}"
 			fmt.Println("- running `" + gox + "` (this might take a while)")
 			Run(gox, projectDir)
 			waitJobs.Done()
